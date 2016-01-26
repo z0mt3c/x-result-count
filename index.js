733f167d2e5ce0b3c9975b3296b60b2a87b724c5
Timo Behrmann <timo.behrmann@gmail.com>
2016-01-26 10:10:18 +0100

    es6

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var parser = module.exports = {
-    parse: function(value) {
-        if (typeof value === 'string') {
-            var indexOfMinus = value.indexOf('-');
-            var indexOfSlash = value.indexOf('/', indexOfMinus);
+'use strict'
 
-            if (indexOfSlash !== -1 && indexOfMinus !== -1) {
-                var skip = parseInt(value.substring(0, indexOfMinus), 10);
-                var stop = parseInt(value.substring(indexOfMinus + 1, indexOfSlash), 10);
-                var total = parseInt(value.substr(indexOfSlash + 1), 10);
-                return {skip: skip, count: stop - skip, total: total};
-            }
-        }
+const parser = module.exports = {
+  parse(value) {
+    if (typeof value === 'string') {
+      const indexOfMinus = value.indexOf('-')
+      const indexOfSlash = value.indexOf('/', indexOfMinus)
 
-        return null;
-    },
-    generate: function(data) {
-        if (data && data.skip >= 0 && data.count >= 0 && data.total >= 0) {
-            var from = data.skip;
-            var to = from + data.count;
-            return from + '-' + to + '/' + data.total;
-        } else {
-            return null;
-        }
+      if (indexOfSlash !== -1 && indexOfMinus !== -1) {
+        const skip = parseInt(value.substring(0, indexOfMinus), 10)
+        const stop = parseInt(value.substring(indexOfMinus + 1, indexOfSlash), 10)
+        const total = parseInt(value.substr(indexOfSlash + 1), 10)
+        return {skip: skip, count: stop - skip, total: total}
+      }
     }
-};
+
+    return null
+  },
+  generate(data) {
+    if (data && data.skip >= 0 && data.count >= 0 && data.total >= 0) {
+      const from = data.skip
+      const to = from + data.count
+      return `${from}-${to}/${data.total}`
+    } else {
+      return null
+    }
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,34 +1,36 @@
-var Code = require('code');
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
-var xResultCount = require('../');
+'use strict'
 
-lab.test('breakthrough', function (done) {
-    var source = {skip: 100, count: 50, total: 999};
-    var generated = xResultCount.generate(source);
-    Code.expect(generated).to.equal('100-150/999');
-    var parsed = xResultCount.parse(generated);
-    Code.expect(parsed).to.deep.equal(source);
-    done();
-});
+const Code = require('code')
+const Lab = require('lab')
+let lab = exports.lab = Lab.script()
+const xResultCount = require('../')
 
-lab.test('parse', function (done) {
-    Code.expect(xResultCount.parse('100-150/999')).to.exist();
-    Code.expect(xResultCount.parse('100-150999')).to.not.exist();
-    Code.expect(xResultCount.parse('100150/999')).to.not.exist();
-    Code.expect(xResultCount.parse(null)).to.not.exist();
-    Code.expect(xResultCount.parse('')).to.not.exist();
-    Code.expect(xResultCount.parse()).to.not.exist();
-    done();
-});
+lab.test('breakthrough', done => {
+  const source = {skip: 100, count: 50, total: 999}
+  const generated = xResultCount.generate(source)
+  Code.expect(generated).to.equal('100-150/999')
+  const parsed = xResultCount.parse(generated)
+  Code.expect(parsed).to.deep.equal(source)
+  done()
+})
 
-lab.test('generate', function (done) {
-    Code.expect(xResultCount.generate({skip: 100, count: 50, total: 999})).to.exist();
-    Code.expect(xResultCount.generate({count: 50, total: 999})).to.not.exist();
-    Code.expect(xResultCount.generate({skip: 100, total: 999})).to.not.exist();
-    Code.expect(xResultCount.generate({skip: 100, count: 50 })).to.not.exist();
-    Code.expect(xResultCount.generate(null)).to.not.exist();
-    Code.expect(xResultCount.generate({})).to.not.exist();
-    Code.expect(xResultCount.generate()).to.not.exist();
-    done();
-});
+lab.test('parse', done => {
+  Code.expect(xResultCount.parse('100-150/999')).to.exist()
+  Code.expect(xResultCount.parse('100-150999')).to.not.exist()
+  Code.expect(xResultCount.parse('100150/999')).to.not.exist()
+  Code.expect(xResultCount.parse(null)).to.not.exist()
+  Code.expect(xResultCount.parse('')).to.not.exist()
+  Code.expect(xResultCount.parse()).to.not.exist()
+  done()
+})
+
+lab.test('generate', done => {
+  Code.expect(xResultCount.generate({skip: 100, count: 50, total: 999})).to.exist()
+  Code.expect(xResultCount.generate({count: 50, total: 999})).to.not.exist()
+  Code.expect(xResultCount.generate({skip: 100, total: 999})).to.not.exist()
+  Code.expect(xResultCount.generate({skip: 100, count: 50})).to.not.exist()
+  Code.expect(xResultCount.generate(null)).to.not.exist()
+  Code.expect(xResultCount.generate({})).to.not.exist()
+  Code.expect(xResultCount.generate()).to.not.exist()
+  done()
+})
